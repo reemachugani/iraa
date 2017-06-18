028bad2b912c9a36c3af992ec933aeca351256cb
Reema Chugani <reemachugani@outlook.com>
2017-06-17 19:21:43 -0700

    changes for staticfiles app

--- config/settings.py
+++ config/settings.py
@@ -139,10 +139,10 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
