ec38cb3f4497cfb75e6da08d9adf607fce99b61a
Reema Chugani <reemachugani@outlook.com>
2017-06-17 21:16:29 -0700

    fixed singlepage templates

--- art/urls.py
+++ art/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'art'
 urlpatterns = [
     url(r'^$', views.art_home, name='art_home'),
+    url(r'^works/(?P<slug>[-\w]+)/$', views.art_detail_page, name='art_detail_page'),
     url(r'^(?P<category_name>[-\w]+)/$', views.art_by_category, name='art_by_category'),
-    url(r'^(?P<slug>[-\w]+)/$', views.art_detail_page, name='art_detail_page'),
 ]
