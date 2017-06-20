5509924c7a3da466628b612e34af765af112f8f7
Reema Chugani <reemachugani@outlook.com>
2017-06-19 20:24:45 -0700

    singlepage admin displays title

--- singlepage/admin.py
+++ singlepage/admin.py
@@ -4,6 +4,7 @@ from .models import Singlepage
 
 class SinglepageAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'slug': ['title'] }
+    list_display = ('title', 'display_word',)
 
     class Media:
         js = ('js/tiny_mce/tiny_mce.js', 'js/tiny_mce/textareas.js',)
